921653e29e49a1030042c55a09febbca80eb8911
HeavyPunk <66221822+HeavyPunk@users.noreply.github.com>
2020-08-06 00:28:42 +0500

    Update neuro.h

--- neuro.h
+++ neuro.h
@@ -1,27 +1,30 @@
+#pragma once
 #ifndef NEURO_H
 #define NEURO_H
+
 #include <vector>
 #include <math.h>
 #include <stdint.h>
 using namespace std;
-class NeuralNet{
-    public:
-        NeuralNet(uint8_t L, uint16_t n[]);
-        double debug_getNeurons(uint16_t neu, uint16_t state, uint16_t layer);
-        double debug_getWeights(uint16_t n1, uint16_t n2, uint8_t L);
-        void Do_it(uint16_t size, double data[]);
-        void getResult(uint16_t size, double *data);
-        void learnBackpropagation(double data, double ans);
-    private:
-        vector<vector<vector<double>>> neurons;
-	    vector<vector<vector<double>>> weights;
-	    uint8_t numLayers=0;
-	    vector<double> neuronsInLayers;
-	    double Func(double in);
-	    double Func_p(double in);
-	    uint32_t MaxEl(uint16_t size, uint16_t arr[]);
-	    void CreateNeurons(uint8_t L, uint16_t n[]);
-	    void CreateWeights(uint8_t L, uint16_t n[]);
+class NeuralNet {
+public:
+    NeuralNet(uint8_t L, uint16_t *n);
+    double debug_getNeurons(uint16_t neu, uint16_t state, uint16_t layer);
+    double debug_getWeights(uint16_t n1, uint16_t n2, uint8_t L);
+    void Do_it(uint16_t size, double *data);
+    void getResult(uint16_t size, double* data);
+    void learnBackpropagation(double* data, double* ans, double acs, double k);
+private:
+    vector<vector<vector<double>>> neurons;
+    vector<vector<vector<double>>> weights;
+    uint8_t numLayers;
+    vector<double> neuronsInLayers;
+    double Func(double in);
+    double Func_p(double in);
+    uint32_t MaxEl(uint16_t size, uint16_t *arr);
+    void CreateNeurons(uint8_t L, uint16_t *n);
+    void CreateWeights(uint8_t L, uint16_t *n);
 
 };
+
 #endif
